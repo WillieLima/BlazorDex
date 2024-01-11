f0f9b1f1f101eea1f15eed63fc269530083f99a2
Wonka <willie.lima_lvs@hotmail.com>
2024-01-11 18:44:37 -0300

    css ajustado, criação da classe PokeColor

---
+++ Models/DreamWorld.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDex.Models
+{
+    public class DreamWorld
+    {
+        public string front_default { get; set; }
+		public object front_female { get; set; }
+    }
+}
---
+++ Models/Other.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDex.Models
+{
+    public class Other
+    {
+        public DreamWorld dream_world { get; set; }
+
+    }
+}
