77df2b5635b07bf7932981fe32536611f256b425
Wonka <willie.lima_lvs@hotmail.com>
2024-01-12 18:18:09 -0300

    Card com a informações dos pokemons criado

--- Util/PokeColor.cs
+++ Util/PokeColor.cs
@@ -11,28 +11,46 @@ namespace BlazorDex.Util
 		{
 			string color = type switch
 			{
-				"bug" => "#26de81",
-				"dragon" => "#ffeaa7",
-				"electric" => "#fed330",
-				"fairy" => "#FF0069",
-				"fighting" => "#30336b",
-				"fire" => "#f0932b",
-				"flying" => "#81ecec",
-				"grass" => "#00b894",
-				"ground" => "#EFB549",
-				"ghost" => "#a55eea",
-				"ice" => "#74b9ff",
-				"normal" => "#95afc0",
-				"poison" => "#6c5ce7",
-				"psychic" => "#a29bfe",
-				"rock" => "#2d3436",
-				"water" => "#0190FF",
-				_ => "#0190FF"
+				"bug" => "#a8b720",
+				"dragon" => "#6f38f6",
+				"electric" => "#f7cf2e",
+				"fairy" => "#f9aec7",
+				"fighting" => "#bf3029",
+				"fire" => "#ee7f30",
+				"flying" => "#a98ff0",
+				"grass" => "#77c850",
+				"ground" => "#dfbf69",
+				"ghost" => "#6e5896",
+				"ice" => "#98d5d7",
+				"normal" => "#a6a877",
+				"poison" => "#a040a0",
+				"psychic" => "#f65687",
+				"rock" => "#b8a137",
+				"water" => "#678fee",
+				"dark" => "#725847",
+				"steel" => "#b9b7cf",
+				_ => "#fff"
 				
 				
 			};
 
 			return color;
 		}
+		public static string GetStatColor(string stat)
+		{
+			string color = stat switch
+			{
+				"hp" => "#FE0000",
+				"attack" => "#EE7F30",
+				"defense" => "#F7D02C",
+				"special-attack" => "#F85687",
+				"special-defense" => "#77C755",
+				"speed" => "#678FEE",
+				_ => "0190FF"
+			};
+
+			return color;
+		}
+		
 	}
 }
